02635d09766553c053742f8d1ec63284dfd776d7
DavidNNussbaum <brocknynj@aol.com>
2021-05-13 18:00:49 -0400

    commit tree 1f086efbccb5a72a1500c292b4728f68dfd79b39

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const record = [
     {year: "2018", result: "L"},
     {year: "2017", result: "W"},
     {year: "2016", result: "N/A"}
-    //...
   ] 
   superbowlWin = (record) => {
-    let result = record.find( record => record.result === "W" )
+    let result = record.find( rec => rec.result === "W" )
     return !!result ? result.year : undefined
   }
-console.log(superbowlWin())
+console.log(superbowlWin(record))
 
  
